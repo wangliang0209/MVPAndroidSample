06c4f00e6652d5ab741b121a81d8adde148edba8
wangliang <wangliang0209@163.com>
2016-10-15 12:06:36 +0800

    test
    Signed-off-by: wangliang <wangliang0209@163.com>

--- app/src/main/java/com/wl/demo/mvpsample/MainActivity.java
+++ app/src/main/java/com/wl/demo/mvpsample/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO
+        //TODO command
 
     }
 
