1b61c35a4d64ad8f7c0717219784c65473453203
wangliang <wangliang0209@163.com>
2016-10-15 12:05:11 +0800

    modify

--- app/src/main/java/com/wl/demo/mvpsample/MainActivity.java
+++ app/src/main/java/com/wl/demo/mvpsample/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO
+
     }
 
     protected void test(View v) {
